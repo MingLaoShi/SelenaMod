f40d84839bcb28d3929849c2f7ede7abb197d082
马老师 <iammaming@126.com>
2026-03-22 10:33:35 +0800

    feat(card): 更新为明天卡牌功能
    - 添加了对奇迹卡牌数量检测逻辑
    - 修改伤害机制为根据奇迹卡牌数量多次执行
    - 更新卡牌描述文本以反映新机制
    - 添加扩展描述显示伤害次数
    - 实现动态描述更新根据升级状态

--- src/main/java/SelenaMod/cards/ForTomorrow.java
+++ src/main/java/SelenaMod/cards/ForTomorrow.java
@@ -4,13 +4,21 @@ import SelenaMod.utils.ModHelper;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ForTomorrow extends CustomSelenaCard {
     public static String ID = ModHelper.makeID("ForTomorrow");
-
+    private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public ForTomorrow() {
         super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
         this.setDamage(10);
@@ -27,7 +35,51 @@ public class ForTomorrow extends CustomSelenaCard {
 
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
-        addToBot(new DamageAllEnemiesAction(abstractPlayer, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+//        addToBot(new DamageAllEnemiesAction(abstractPlayer, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
         addToBot(new MakeTempCardInDrawPileAction(this.cardsToPreview, this.magicNumber, false, false, true));
+        for (int i = 0; i < this.magicNumber; i++) {
+            addToBot(new DamageAllEnemiesAction(abstractPlayer, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        }
+    }
+
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+        this.magicNumber = baseMagicNumber + findMiracles();
+        this.isMagicNumberModified = magicNumber != baseMagicNumber;
+        if (!this.upgraded) {
+            this.rawDescription = cardStrings.DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0];
+        } else {
+            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0];
+        }
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        super.calculateCardDamage(mo);
+        this.magicNumber = baseMagicNumber + findMiracles();
+        this.isMagicNumberModified = magicNumber != baseMagicNumber;
+        if (!this.upgraded) {
+            this.rawDescription = cardStrings.DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0];
+        } else {
+            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0];
+        }
+    }
+
+    private int findMiracles() {
+        int count = 0;
+        List<CardGroup> groups = new ArrayList<>();
+        groups.add(AbstractDungeon.player.drawPile);
+        groups.add(AbstractDungeon.player.discardPile);
+        groups.add(AbstractDungeon.player.hand);
+        groups.add(AbstractDungeon.player.exhaustPile);
+        for (CardGroup g : groups) {
+            for (AbstractCard c : g.group) {
+                if (c instanceof Miracle) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
